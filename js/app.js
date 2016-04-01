f68ba20847d7cc9e6278438010b73839481a6acf
David Quigley <daquigley4@gmail.com>
2016-04-01 16:52:38 -0400

    created wireframe, finished basic game logic

--- js/app.js
+++ js/app.js
@@ -2,10 +2,28 @@
 $(document).ready(function() {
 
   var $gameCells = $('.cell');
+  var moves = ["", "", "", "", "", "", "", "", ""];
+  // var moves = Array(9).join(".").split(".");
+  var counter = 0;
+  var turn = 'X';
 
   $($gameCells).one('click', function() {
     console.log('Gameboard clicked!');
+    // moves[this.id] = (counter % 2 == 0)? 'X' : 'O';
+    // moves[this.id] = this.id;
+    $(this).html(turn);
+    moves[this.id] = turn;
+    counter++;
+    if (counter % 2 == 0) {
+      turn = 'X';
+    }
+    else {
+      turn = 'O';
+    }
+
     console.log(this.id);
+    console.log(moves, counter, turn);
+
   });
 
 });
